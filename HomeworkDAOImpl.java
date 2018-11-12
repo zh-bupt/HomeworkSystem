7c305e1b7674da9069c7acae8519d491411735aa
I4cream <44915750+I4cream@users.noreply.github.com>
2018-11-12 13:36:24 +0800

    Delete HomeworkDAOImpl.java

--- HomeworkDAOImpl.java
+++
@@ -1,7 +0,0 @@
-package com.bupt.se.homework.dao.impl;
-
-import com.bupt.se.homework.dao.HomeworkDAO;
-import com.bupt.se.homework.entity.Homework
-
-public class HomeworkDAOImpl extends BasicDAOImpl implements HomeworkDAO {
-}
