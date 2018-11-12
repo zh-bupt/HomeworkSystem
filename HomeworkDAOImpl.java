8f4de8f7ec3f358bf8df5bfbee4f9730b170b2a1
I4cream <44915750+I4cream@users.noreply.github.com>
2018-11-12 13:33:30 +0800

    Add files via upload

---
+++ HomeworkDAOImpl.java
@@ -0,0 +1,7 @@
+package com.bupt.se.homework.dao.impl;
+
+import com.bupt.se.homework.dao.HomeworkDAO;
+import com.bupt.se.homework.entity.Homework
+
+public class HomeworkDAOImpl extends BasicDAOImpl implements HomeworkDAO {
+}
